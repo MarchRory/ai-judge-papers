5dd40b1cbcad6ea68e95ed05c33b94268130572e
LiuShi_2 <2287722838@qq.com>
2024-04-12 18:35:52 +0800

    feat: 预设问题, 细节调整

--- src/router/routes/index.ts
+++ src/router/routes/index.ts
@@ -1,9 +1,12 @@
 import { AppRouteRecordRaw } from './types';
 
+// 暂时不渲染出来的路由, 把name配置在这里
+const notUse = ['permission-mgmt', 'subject-mgmt'];
+
 export default function formatModules(_modules: any, result: AppRouteRecordRaw[]) {
   Object.keys(_modules).forEach((key) => {
     const defaultModule = _modules[key].default;
-    if (!defaultModule) return;
+    if (!defaultModule || notUse.includes(defaultModule.name)) return;
     const moduleList = Array.isArray(defaultModule) ? [...defaultModule] : [defaultModule];
     result.push(...moduleList);
   });
